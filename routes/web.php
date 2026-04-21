0c587ff5c188c00f620d87c306d1bd1240bb8d96
Phucnguyen8625 <phucdu969@gmail.com>
2026-04-21 13:00:25 +0700

    Cập nhật cuối cùng: Sửa lỗi 404 /login và dọn dẹp cấu hình .env

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\SocialAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'auth.login')->name('login');
+Route::redirect('/login', '/');
 
 Route::get('/auth/{provider}/redirect', [SocialAuthController::class, 'redirect'])
     ->where('provider', 'google|facebook')
